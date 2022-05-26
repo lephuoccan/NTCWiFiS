c1407f63ad7924a1f192a68c7dcfe696b83eb525
lephuoccan <lephuoccan@gmail.com>
2022-05-26 15:33:24 +0700

    ESPNOW
    Update espnow

--- NTCWiFiS/NTCWiFiS.ino
+++ NTCWiFiS/NTCWiFiS.ino
@@ -3,19 +3,17 @@ extern "C"{
 }
 #include "BoardCFG.h"
 #include "MCP3208.h"
+#include <WiFi.h>
 #include <driver/adc.h>
+#include <esp_WiFi.h>
+#include <esp_now.h>
 
 #define NSS 26
-static const int spiClk = 1000000; // 1 MHz
+static const int spiClk = 1000000;   /*SPI clock 1 MHz*/
 iMCP3208 ADCi(14,12,13,NSS,spiClk);
-const float _Vref = 3300;  
+const float _Vref = 3300;            /*Vref mV*/
 const float _ADC_MAX = 4095;         /* ADC 12bit */
-const float _T0 = 25.0;              /*  */
-float _NTC_BETA = 3950.0;
-
-const float _R0 = 100000.0;
 const float _R1 = 10000.0;
-
 const float _R25 = 100000.0;
 const float _R50 = 35899.9;
 const float _R100 = 6710.0;
@@ -41,15 +39,19 @@ float TC_K_Temperature;
 uint32_t smps,c_smps;
 uint32_t micro;
 /*ADC Variables*/
-
-
-const int MultiSample_num = 64;
-int ADC_Sampling_Value[MultiSample_num];
-/***************/
-int getMostPopularElement(int arr[], const int n);
-void Switch_ADC_Pin(int ADC_Pin);
-int Read_ADC_MultiSampling(int Pin);
+/********************************************************************************/
+/*ESP NOW Variables*/
+esp_now_peer_info_t peerInfo;
+uint8_t Broadcast_MAC[]={0xFF,0xFF,0xFF,0xFF,0xFF,0xFF};
+uint8_t Master_MAC[]={0x24,0x62,0xAB,0xE4,0x06,0x20};
+const uint8_t outcomData_maxlen=250;
+uint8_t outcomData[outcomData_maxlen];
+uint8_t incomData[250];
+uint8_t ESPNOW_Data_Received;
+/********************************************************************************/
+void InitESPNow();
 float map_value(float x, float in_min, float in_max, float out_min, float out_max);
+
 void ADC_Process(void * parameter)
 {
   vTaskDelay(1000 / portTICK_PERIOD_MS); /*Delay 1000ms*/
@@ -58,22 +60,22 @@ void ADC_Process(void * parameter)
   {
     NTC1.ADC_Raw = ADCi.read(SINGLE_7);
     NTC1.ADC_Value = Comfilter*NTC1.ADC_Value + (1-Comfilter)*NTC1.ADC_Raw;
-    NTC1.Resistance =((float)(NTC1.ADC_Value)*_ADC_MAX*10000)/(_ADC_MAX*(_ADC_MAX-(float)NTC1.ADC_Value));
+    NTC1.Resistance =((float)(NTC1.ADC_Value)*_ADC_MAX*_R1)/(_ADC_MAX*(_ADC_MAX-(float)NTC1.ADC_Value));
     NTC1.Temperature_C = R_To_Temperature(NTC1.Resistance);
 
     NTC2.ADC_Raw = ADCi.read(SINGLE_6);
     NTC2.ADC_Value = Comfilter*NTC2.ADC_Value + (1-Comfilter)*NTC2.ADC_Raw;
-    NTC2.Resistance =((float)(NTC2.ADC_Value)*_ADC_MAX*10000)/(_ADC_MAX*(_ADC_MAX-(float)NTC2.ADC_Value));
+    NTC2.Resistance =((float)(NTC2.ADC_Value)*_ADC_MAX*_R1)/(_ADC_MAX*(_ADC_MAX-(float)NTC2.ADC_Value));
     NTC2.Temperature_C = R_To_Temperature(NTC2.Resistance);
 
     NTC3.ADC_Raw = ADCi.read(SINGLE_5);
     NTC3.ADC_Value = Comfilter*NTC3.ADC_Value + (1-Comfilter)*NTC3.ADC_Raw;
-    NTC3.Resistance =((float)(NTC3.ADC_Value)*_ADC_MAX*10000)/(_ADC_MAX*(_ADC_MAX-(float)NTC3.ADC_Value));
+    NTC3.Resistance =((float)(NTC3.ADC_Value)*_ADC_MAX*_R1)/(_ADC_MAX*(_ADC_MAX-(float)NTC3.ADC_Value));
     NTC3.Temperature_C = R_To_Temperature(NTC3.Resistance);
 
     NTC4.ADC_Raw = ADCi.read(SINGLE_4);
     NTC4.ADC_Value = Comfilter*NTC4.ADC_Value + (1-Comfilter)*NTC4.ADC_Raw;
-    NTC4.Resistance =((float)(NTC4.ADC_Value)*_ADC_MAX*10000)/(_ADC_MAX*(_ADC_MAX-(float)NTC4.ADC_Value));
+    NTC4.Resistance =((float)(NTC4.ADC_Value)*_ADC_MAX*_R1)/(_ADC_MAX*(_ADC_MAX-(float)NTC4.ADC_Value));
     NTC4.Temperature_C = R_To_Temperature(NTC4.Resistance);
 
     PS1.ADC_Raw = ADCi.read(SINGLE_0);
@@ -98,17 +100,35 @@ void ADC_Process(void * parameter)
 //    vTaskDelay(1 / portTICK_PERIOD_MS); /*Delay 1000ms*/
   }
 }
+void OnDataSent(const uint8_t *mac_addr, esp_now_send_status_t status) {
+  char macStr[18];
+  snprintf(macStr, sizeof(macStr), "%02x:%02x:%02x:%02x:%02x:%02x",
+           mac_addr[0], mac_addr[1], mac_addr[2], mac_addr[3], mac_addr[4], mac_addr[5]);
+//  Serial.print("Last Packet Sent to: "); Serial.println(macStr);
+//  Serial.print("Last Packet Send Status: "); Serial.println(status == ESP_NOW_SEND_SUCCESS ? "Delivery Success" : "Delivery Fail");
+}
 void setup() {
   // put your setup code here, to run once:
   UART_Debug.begin(115200);
-  delay(500);
-  
-  ADCi.begin();
-  UART_Debug.println("Hello World");
+  delay(1000);
+  UART_Debug.println("Initialize UART_debug: UART0 115200 8N1");
+  UART_Debug.println(WiFi.macAddress());
   UART_Debug.printf("Setup is running on CPU %d\n", xPortGetCoreID());
   
+  ADCi.begin();
+  esp_wifi_set_ps(WIFI_PS_NONE);    /* No power save */
   PS1.Resistance = 150.0;
   PS2.Resistance = 150.0;
+  
+  InitESPNow();
+  esp_now_register_send_cb(OnDataSent);
+  memcpy(peerInfo.peer_addr, Master_MAC, 6);
+  peerInfo.channel = 1;  
+  peerInfo.encrypt = false;     
+  if (esp_now_add_peer(&peerInfo) != ESP_OK){
+    UART_Debug.println("Failed to add peer");
+    return;
+  }
   
   xTaskCreatePinnedToCore(
     ADC_Process,
@@ -123,99 +143,39 @@ void setup() {
 
 void loop() {
   // put your main code here, to run repeatedly:
+  static uint32_t tick_espnow = 0;
+  if(millis() - tick_espnow >= 1000)
+  {
+    memset(outcomData,0,outcomData_maxlen);
+    sprintf((char*)outcomData,"NTCPS:%.2f %.2f %.2f %.2f",NTC1.Temperature_C,NTC2.Temperature_C,NTC3.Temperature_C,NTC4.Temperature_C);
+    UART_Debug.println((char*)outcomData);
+    esp_err_t result = esp_now_send(Master_MAC, (uint8_t *) &outcomData[0], strlen((char*)outcomData));
+    if (result != ESP_OK)
+    {
+      UART_Debug.print("Send Status: ");
+      if (result == ESP_ERR_ESPNOW_NOT_INIT) {
+        // How did we get so far!!
+        UART_Debug.println("ESPNOW not Init.");
+      } else if (result == ESP_ERR_ESPNOW_ARG) {
+        UART_Debug.println("Invalid Argument");
+      } else if (result == ESP_ERR_ESPNOW_INTERNAL) {
+        UART_Debug.println("Internal Error");
+      } else if (result == ESP_ERR_ESPNOW_NO_MEM) {
+        UART_Debug.println("ESP_ERR_ESPNOW_NO_MEM");
+      } else if (result == ESP_ERR_ESPNOW_NOT_FOUND) {
+        UART_Debug.println("Peer not found.");
+      } else {
+        UART_Debug.println("Not sure what happened");
+      }
+    }
+    tick_espnow = millis();
+  }
 //  UART_Debug.printf("ADC  = %d %d\n",NTC1.ADC_Raw,(int)NTC1.ADC_Value);
   UART_Debug.printf("T = %f   %f   %f   %f    %f\n", NTC1.Temperature_C, NTC2.Temperature_C, NTC3.Temperature_C, NTC4.Temperature_C, TC_K_Temperature);
 //  UART_Debug.printf("ADC  = %f %f   %f %f\n",PS1.Voltage,PS1.Current,PS2.Voltage,PS2.Current );
   vTaskDelay(100 / portTICK_PERIOD_MS); /*Delay 1000ms*/
 }
 
-int getMostPopularElement(int arr[], const int n)
-{
-    int count = 1, tempCount;
-    int temp = 0,i = 0,j = 0;
-    //Get first element
-    int popular = arr[0];
-    for (i = 0; i < (n- 1); i++)
-    {
-        temp = arr[i];
-        tempCount = 0;
-        for (j = 1; j < n; j++)
-        {
-            if (temp == arr[j])
-                tempCount++;
-        }
-        if (tempCount > count)
-        {
-            popular = temp;
-            count = tempCount;
-        }
-    }
-    return popular;
-}
-void Switch_ADC_Pin(int ADC_Pin)
-{
-  if(ADC_Pin == NTC1_Pin)
-  {
-    digitalWrite(SA0_Pin, LOW);
-    digitalWrite(SA1_Pin, LOW);
-    digitalWrite(SA2_Pin, LOW);
-  }
-  else if(ADC_Pin == NTC2_Pin)
-  {
-    digitalWrite(SA0_Pin, HIGH);
-    digitalWrite(SA1_Pin, LOW);
-    digitalWrite(SA2_Pin, LOW);
-  }
-  else if(ADC_Pin == NTC3_Pin)
-  {
-    digitalWrite(SA0_Pin, LOW);
-    digitalWrite(SA1_Pin, HIGH);
-    digitalWrite(SA2_Pin, LOW);
-  }
-  else if(ADC_Pin == NTC4_Pin)
-  {
-    digitalWrite(SA0_Pin, HIGH);
-    digitalWrite(SA1_Pin, HIGH);
-    digitalWrite(SA2_Pin, LOW);
-  }
-  else if(ADC_Pin == PS1_Pin)
-  {
-    digitalWrite(SA0_Pin, LOW);
-    digitalWrite(SA1_Pin, LOW);
-    digitalWrite(SA2_Pin, HIGH);
-  }
-  else if(ADC_Pin == PS2_Pin)
-  {
-    digitalWrite(SA0_Pin, HIGH);
-    digitalWrite(SA1_Pin, LOW);
-    digitalWrite(SA2_Pin, HIGH);
-  }
-  else if(ADC_Pin == PWR_Pin)
-  {
-    digitalWrite(SA0_Pin, LOW);
-    digitalWrite(SA1_Pin, HIGH);
-    digitalWrite(SA2_Pin, HIGH);
-  }
-  else if(ADC_Pin == VREF_Pin)
-  {
-    digitalWrite(SA0_Pin, HIGH);
-    digitalWrite(SA1_Pin, HIGH);
-    digitalWrite(SA2_Pin, HIGH);
-  }
-}
-int Read_ADC_MultiSampling(int Pin)
-{
-  int ADC_Buff[MultiSample_num];
-  int retVal;
-  Switch_ADC_Pin(Pin);
-  vTaskDelay(1 / portTICK_PERIOD_MS); /*Delay 1ms*/
-  for(int i = 0; i< MultiSample_num; i++)
-  {
-    ADC_Buff[i] = analogRead(ADC_pin);
-  }
-  retVal = getMostPopularElement(ADC_Buff, MultiSample_num);
-  return retVal;
-}
 float map_value(float x, float in_min, float in_max, float out_min, float out_max)
 {
   return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
@@ -256,4 +216,27 @@ float R_To_Temperature(float Resistance)
   }
   retVal = (Beta*(273.15 + Tb))/(Beta+((273.15+ Tb)*log(Resistance/Rb)))-273.15;
   return retVal;
+}
+void InitESPNow() {
+  WiFi.mode(WIFI_STA);
+  WiFi.disconnect();
+  if (esp_now_init() == ESP_OK) {
+    UART_Debug.println("ESPNow Init Success");
+  }
+  else {
+    UART_Debug.println("ESPNow Init Failed");
+    // Retry InitESPNow, add a counte and then restart?
+    // InitESPNow();
+    // or Simply Restart
+    ESP.restart();
+  }
+  esp_wifi_set_channel(1,WIFI_SECOND_CHAN_NONE);
+}
+
+void data_receive(const uint8_t * mac, const uint8_t *incomingData, int len) {
+  memset(&incomData[0],0,250);
+  memcpy(&incomData[0],incomingData,len);
+//  ESPNOW_Data_len = len;
+//  ESPNOW_Data_Received = 1;
+
 }
